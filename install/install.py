15c7ed23a6934f3158ef50e0814754f8c1895cad
Sw1mmeR <scriptdefender@yandex.ru>
2022-12-13 19:42:47 +0700

    Fix install

--- install/install.py
+++ install/install.py
@@ -7,22 +7,22 @@ from xipraylib.files_holder import is_sudo, set_os_paths, install
 from xipraylib.xstdout import print_param
 from sys import platform
 
-logger = get_logger(__name__)
+#logger = get_logger(__name__)
 
 if(__name__ == '__main__'):
-    logger.info('Starting script installation')
+    #logger.info('Starting script installation')
     
     try:
         if(platform == "linux" or platform == "linux2"):
-            logger.info('Linux system detected')
+            #logger.info('Linux system detected')
             is_sudo()
             os.system('./install.sh')
         elif(platform == 'win32'):
-            logger.info('Windows system detected')
+            #logger.info('Windows system detected')
             os.system('./install.bat')
     except Exception as ex:
         print_param(ex, mode='error')
         exit(10)
     set_os_paths()
     install()
-    logger.info('Installation finished')
+    #logger.info('Installation finished')
